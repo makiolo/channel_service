fa7295b590df6848e9eabd9d0faaf7699192d91e
Ricardo <makiolo@gmail.com>
2017-03-13 13:34:17 +0100

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -1,21 +1,3 @@
 #include "node.h"
 //
 #include <iostream>
-
-using namespace RakNet;
-
-namespace ser {
-
-node::node(const ser::string& alias, uint16 port)
-	: _alias(alias)
-	, _server(alias, port)
-{
-	;
-}
-
-node::~node()
-{
-	;
-}
-
-}
