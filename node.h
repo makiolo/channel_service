c106c12b1a3444724cacde1309f71cb3bd1abb5b
Ricardo <makiolo@gmail.com>
2017-03-13 13:33:09 +0100

    Update node.h

--- node.h
+++ node.h
@@ -13,9 +13,24 @@ public:
 	virtual ~node();
 	node(const node&) = delete;
 	node& operator=(const node&) = delete;
+	
+	void connect(const std::string& host, uint16 port)
+	{
+		_clients.emplace_back(_alias + "_client", host, port);
+	}
 
+	template <typename ... Args>
+	void operator()(int port, int version, Args&& ... data) const
+	{
+		for(auto& client : _clients)
+		{
+			client.up(port, version, std::forward<Args>(data)...)
+		}
+	}
+	
 protected:
-	ser::client _client;
+	ser::string _alias;
+	std::vector<ser::client> _clients;
 	ser::network_server _server;
 };
 
